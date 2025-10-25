9ff7c4f688d9ab541a1eab969b2509fb9d3b37c3
HarshJani1 <harshjani011@gmail.com>
2025-10-25 11:05:48 +0530

    Fixed Filter.

--- api-gateway/src/main/java/com/grocery/api_gateway/Filters/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/grocery/api_gateway/Filters/AuthenticationFilter.java
@@ -2,58 +2,55 @@ package com.grocery.api_gateway.Filters;
 
 import com.grocery.api_gateway.Util.JwtUtil;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @Component
-public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+public class AuthenticationFilter implements GlobalFilter {
 
     private final RouteValidator validator;
 
     private final JwtUtil jwtUtil;
 
     public AuthenticationFilter(RouteValidator validator, JwtUtil jwtUtil) {
-        super(Config.class);
+        super();
         this.validator = validator;
         this.jwtUtil = jwtUtil;
     }
 
     @Override
-    public GatewayFilter apply(Config config) {
-        return (exchange, chain) -> {
-            String path = exchange.getRequest().getURI().getPath();
-            boolean secured = validator.isSecured.test(exchange.getRequest());
-            System.out.println("AuthenticationFilter - Request Path: " + path + " | isSecured: " + secured);
+    public Mono<Void> filter(ServerWebExchange exchange, org.springframework.cloud.gateway.filter.GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+        boolean secured = validator.isSecured.test(exchange.getRequest());
+        System.out.println("AuthenticationFilter - Path: " + path + " | isSecured: " + secured);
 
-            if (secured) {
-                // header contains token or not
-                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-                    exchange.getResponse().setStatusCode(org.springframework.http.HttpStatus.UNAUTHORIZED);
-                    return exchange.getResponse().setComplete();
-                }
-
-                String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-                if (authHeader != null && authHeader.startsWith("Bearer ")) {
-                    authHeader = authHeader.substring(7);
-                }
-
-                try {
-                    jwtUtil.validateToken(authHeader);
-                } catch (Exception e) {
-                    System.out.println("invalid access...!: " + e.getMessage());
-                    exchange.getResponse().setStatusCode(org.springframework.http.HttpStatus.UNAUTHORIZED);
-                    return exchange.getResponse().setComplete();
-                }
+        if (secured) {
+            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
             }
 
-            return chain.filter(exchange);
+            String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                authHeader = authHeader.substring(7);
+            }
+
+            try {
+                jwtUtil.validateToken(authHeader);
+            } catch (Exception e) {
+                System.out.println("Invalid access token: " + e.getMessage());
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+        }
+
+        return chain.filter(exchange);
         };
-    }
-
-
-    public static class Config {
-
-    }
 }
+
